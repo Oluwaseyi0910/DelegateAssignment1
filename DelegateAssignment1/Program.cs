08df2c59558ad996149a562a917a68da154f0347
Oluwaseyi0910 <110403016+Oluwaseyi0910@users.noreply.github.com>
2022-08-16 08:29:54 +0100

    Add project files.

---
+++ DelegateAssignment1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAssignment1
+{
+    /// <summary>
+    /// This is the Predicate Class
+    /// </summary>
+    internal class Delegates1
+    {
+        public delegate bool Predicate(XClass x);
+
+        void LambdaExpansion()
+        {
+
+            var likelyUsername = "Oluwaseyi";
+
+            Predicate classwork = (XClass x) =>
+            {
+                if (x.UserName == likelyUsername)  //check if Username is equal to LikelyUsername
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+
+
+    /// <summary>
+    /// This is the property class
+    /// </summary>
+    class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
+
+
